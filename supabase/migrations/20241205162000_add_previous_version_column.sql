965cf28ad82ec85f2080641c64582a3832c66240
AndyEganCo <andy@andyegan.co>
2025-12-12 16:33:46 -0600

    Removed old Mirgration

--- supabase/migrations/20241205162000_add_previous_version_column.sql
+++
@@ -1,9 +0,0 @@
--- Add previous_version column to software_version_history
--- This tracks what version was replaced by the new version
-
-ALTER TABLE software_version_history
-  ADD COLUMN IF NOT EXISTS previous_version TEXT;
-
--- Add index for efficient querying
-CREATE INDEX IF NOT EXISTS idx_software_version_history_previous_version
-  ON software_version_history(previous_version);
